90cebd08766821ffd35ae726a6434a45ed36d97d
Grzegorz (Greg) Haranczyk <gzharanczyk@gmail.com>
2015-04-16 15:31:37 -0400

    Delete views.py

--- views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, render_to_response, RequestContext
-
-# Create your views here.
-def home(request):
-    return render_to_response("join.html", locals(), context_instance=RequestContext(request))
